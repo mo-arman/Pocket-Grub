bee36ee166c3be29147a69dc4e6b3a1f149e188c
mo-arman <moharman3818@gmail.com>
2024-09-25 15:35:07 +0530

    Get GitHub user Api

--- src/component/UserClass.js
+++ src/component/UserClass.js
@@ -3,27 +3,42 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-   
+
     // create state varibale in class Based Component
     // this state is big object
     this.state = {
       count: 0,
       count2: 1,
+      UserInfo:{
+        name:"Dummy",
+        location:"Default",
+        avatar_url:"Default",
+        bio:"Default",
+      }
     };
     // console.log(props);
     console.log("Child Constructor");
   }
-  componentDidMount(){
-    console.log("Child ComponentDidMount");
+  async componentDidMount() {
+    // console.log("Child ComponentDidMount");
+    const data = await fetch("https://api.github.com/users/mo-arman");
+    const json = await data.json();
+    console.log(json);
+
+    this.setState({
+        UserInfo:json,
+    });
   }
   render() {
-    const { name, location } = this.props;
+    const { name, location,avatar_url } = this.props;
     const { count, count2 } = this.state;
     console.log("Child render");
     return (
       <div className="about">
-        <h1>Name:{name}</h1>
-        <h2>Location:{location}</h2>
+        <img src={this.state.UserInfo.avatar_url}/>
+        <h1>Name:{this.state.UserInfo.name}</h1>
+        <h2>Location:{this.state.UserInfo.location}</h2>
+        <h2>Bio:{this.state.UserInfo.bio}</h2>
         <h3>Class Based Component</h3>
         <h2>Count:{count}</h2>
         <button
