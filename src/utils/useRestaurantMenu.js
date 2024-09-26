a53a4227a45c89f16c025e3857d92a3bf98db3dd
mo-arman <moharman3818@gmail.com>
2024-09-26 09:09:21 +0530

    create own CutomeHook Fetching logic own cutomehook

---
+++ src/utils/useRestaurantMenu.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { RESID_LINK } from "../utils/constant";
+
+
+const useRestaurantMenu = (resId) => {
+    const [resInfo,setResInfo] = useState(null);
+    // fetchData
+
+    useEffect(()=>{
+        fetchData();
+
+    },[]);
+    const fetchData = async () => {
+        const data = await fetch(RESID_LINK+resId);
+        const json = await data.json();
+        setResInfo(json?.data);
+
+    }
+    return resInfo;
+}
+
+export default useRestaurantMenu;
