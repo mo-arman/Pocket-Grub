fa4a1075c96821d1e1de4d7b634c98f8a5e7966c
mo-arman <moharman3818@gmail.com>
2024-09-26 08:46:50 +0530

    Class

--- src/component/UserClass.js
+++ src/component/UserClass.js
@@ -9,33 +9,36 @@ class UserClass extends React.Component {
     this.state = {
       count: 0,
       count2: 1,
-      UserInfo:{
-        name:"Dummy",
-        location:"Default",
-        avatar_url:"Default",
-        bio:"Default",
-      }
+      UserInfo: {
+        name: "Dummy",
+        location: "Default",
+        avatar_url: "Default",
+        bio: "Default",
+      },
     };
     // console.log(props);
     console.log("Child Constructor");
   }
   async componentDidMount() {
     // console.log("Child ComponentDidMount");
+    // Api Call
     const data = await fetch("https://api.github.com/users/mo-arman");
     const json = await data.json();
     console.log(json);
 
     this.setState({
-        UserInfo:json,
+      UserInfo: json,
     });
   }
   render() {
-    const { name, location,avatar_url } = this.props;
+    const { name, location, avatar_url } = this.props;
     const { count, count2 } = this.state;
+
     console.log("Child render");
+    
     return (
       <div className="about">
-        <img src={this.state.UserInfo.avatar_url}/>
+        <img src={this.state.UserInfo.avatar_url} />
         <h1>Name:{this.state.UserInfo.name}</h1>
         <h2>Location:{this.state.UserInfo.location}</h2>
         <h2>Bio:{this.state.UserInfo.bio}</h2>
