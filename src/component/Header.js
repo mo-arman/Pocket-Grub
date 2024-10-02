df6305b3a1b6a5c9d13f99cc3a287a0ef165b587
mo-arman <moharman3818@gmail.com>
2024-10-02 07:49:56 +0530

    Add Tailwindcss  Header page

--- src/component/Header.js
+++ src/component/Header.js
@@ -10,42 +10,50 @@ const Header = () => {
   // let btnName = "Login";
   // let btnName = "Login";
   return (
-    <div className="flex  justify-between bg-pink-200 h-28 shadow-xl mb-">
+    <div className="flex  justify-between bg-pink-400 h-28 shadow-2xl rounded-lg mt-1 ml-2 mr-2 ">
       <div className="">
         <img className="w-52 absolute top-[-7%]" src={LOGO} alt="LOGO_URL" />
       </div>
       <div className="flex items-center">
         <div className="">
-        <ul className="flex p-4 m-4">
-          <li className="px-10 font-bold text-2xl">
-            Online Status: {onlineStatus ? "🌍" : "🐍"}
-          </li>
-          <li className="px-4 font-bold text-xl  hover:bg-pink-400 rounded-lg">
-            <Link to="/">Home</Link>
+          <ul className="flex p-4 m-4">
+            <li className="px-10 font-bold text-2xl">
+              Online Status: {onlineStatus ? "🌍" : "🐍"}
             </li>
-          <li className="px-4 font-bold text-xl hover:bg-pink-400 rounded-lg"> <Link to="/contact">Contact Us</Link></li>
-          <li className="px-4 font-bold text-xl  hover:bg-pink-400 rounded-lg"> <Link to="/about">About Us</Link></li>
-          <li className="px-4 font-bold text-xl  hover:bg-pink-400 rounded-lg"> <Link to="/cart">Cart</Link></li>
-          <div className="px-4">
-          <button
-            className="bg-green-300 px-2 py-2 rounded-lg pt-1 font-bold hover:bg-pink-400"
-            onClick={() => {
-              // btnName = "Logout" ? btnName = "Logout" : btnName = "Login";
-              // console.log(btnName);
-              btnNameReact == "Login"
-                ? setBtnNameReact("Logout")
-                : setBtnNameReact("Login");
-              console.log(btnNameReact);
-            }}
-          >
-            {btnNameReact}
-          </button>
-          </div>
-        </ul>
+            <li className="px-4 font-bold text-xl  hover:bg-purple-400 rounded-lg">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="px-4 font-bold text-xl hover:bg-purple-400 rounded-lg">
+              {" "}
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <li className="px-4 font-bold text-xl  hover:bg-purple-400 rounded-lg">
+              {" "}
+              <Link to="/about">About Us</Link>
+            </li>
+            <li className="px-4 font-bold text-xl  hover:bg-purple-400 rounded-lg">
+              {" "}
+              <Link to="/cart">Cart</Link>
+            </li>
+            <div className="px-4">
+              <button
+                className="bg-green-300 px-2 py-2 rounded-lg pt-1 font-bold hover:bg-purple-400 "
+                onClick={() => {
+                  // btnName = "Logout" ? btnName = "Logout" : btnName = "Login";
+                  // console.log(btnName);
+                  btnNameReact == "Login"
+                    ? setBtnNameReact("Logout")
+                    : setBtnNameReact("Login");
+                  console.log(btnNameReact);
+                }}
+              >
+                {btnNameReact}
+              </button>
+            </div>
+          </ul>
         </div>
       </div>
     </div>
-   
   );
 };
 
--- src/component/ShimmerCard.js
+++ src/component/ShimmerCard.js
@@ -1,26 +1,72 @@
-import React from 'react'
+import React from "react";
 
 const ShimmerCard = () => {
   return (
-    <div className="shimmer-container">
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
-      <div className="card">Card</div>
+    <div className="ml-[45px] mt-16">
+      <div className="flex flex-wrap m-4 rounded-lg mt-8 ">
+        <div
+          className="w-[230px]  shadow-xl h-[300px] m-4 rounded-lg "
+          style={{ backgroundColor: "#f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+        <div
+          className="w-[230px] shadow-xl h-[300px] m-4 rounded-lg"
+          style={{ backgroundColor: "#f0f0f0f0" }}
+        ></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ShimmerCard;
