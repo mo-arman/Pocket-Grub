ff7fe89777565da107cff4d30198aa08ffc6989a
mo-arman <moharman3818@gmail.com>
2024-10-02 07:49:25 +0530

    css contact page

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,11 +1,145 @@
-
-
 const Contact = () => {
   return (
-    <div className="h-16 w-16 border border-black rounded-lg">
-      <h1>Contact</h1>
-    </div>
-  )
-}
+  //   <div className="w-[500px]  h-[600px] mt-4 ml-[100px] shadow-lg ">
+  //     <div className="w-[500px] h-[140px] bg-blue-400"></div>
+  //     <div className=" border border-black h-[450px] shadow-lg">
+  //     <div className=" rounded-lg absolute bottom-[350px] left-[115px]  bg-white  w-[470px] ">
+  //       <div className=" ml-[180px] mt-4  ">
+  //         <h1 className="font-bold text-xl font-sans">Get in Touch</h1>
+  //         <div className="absolute left-[160px] mt-4">
+  //           <div className="  w-[380px] ">
+  //             <p className="font-mono text-[10px]">
+  //               Nunc erat cursur tellus gravida
+  //             </p>
+  //           </div>
+  //         </div>
+  //       </div>
+  //       <div>
+  //         <div className="mt-12  ">
+  //           <form>
+  //             <label className="absolute left-[10px] font-bold" for="fName">
+  //               First Name:
+  //             </label>
+  //             <br></br>
+  //             <input
+  //               className="absolute left-[10px] mt-2 border border-blue-600 w-[450px] rounded-md"
+  //               type="text"
+  //               placeholder="Please Enter First Name"
+  //             />
+  //             <br></br>
+  //             <label
+  //               className="absolute left-[10px] mt-3 font-bold"
+  //               for="lname"
+  //             >
+  //               Last name:
+  //             </label>
+  //             <br></br>
+  //             <input
+  //               className="absolute left-[10px] mt-4 border border-blue-600 w-[450px] rounded-md"
+  //               type="text"
+  //               placeholder="Please Enter First Name"
+  //             ></input>
 
-export default Contact
+  //             <label
+  //               className="absolute left-[10px] mt-12 font-bold"
+  //               for="lname"
+  //             >
+  //               Email:
+  //             </label>
+  //             <br></br>
+  //             <input
+  //               className="absolute left-[10px] mt-14 border border-blue-600 w-[450px] rounded-md"
+  //               type="text"
+  //               placeholder="Please Enter First Name"
+  //             ></input>
+
+  //             <label
+  //               className="absolute left-[10px] mt-[90px] font-bold"
+  //               for="lname"
+  //             >
+  //               Phone Number:
+  //             </label>
+  //             <br></br>
+  //             <input
+  //               className="absolute left-[10px] mt-[97px] border border-blue-600 w-[450px] rounded-md"
+  //               type="text"
+  //               placeholder="Please Enter First Name"
+  //             ></input>
+  //           </form>
+  //           <button className="font-bold text-black bg-blue-500 absolute bottom-[-200px] py-2 px-[200px] rounded-lg left-2">
+  //             Submit
+  //           </button>
+  //         </div>
+  //       </div>
+  //     </div>
+  //     </div>
+  //   </div>
+  <div className="w-[550px]  h-[650px] absolute left-[100px] mt-5 shadow-2xl">
+    <div className="w-[550px]  h-[150px] bg-blue-400">
+      <div className=" w-[500px] h-[550px] absolute left-[22px] top-[50px] rounded-lg bg-white shadow-2xl">
+        <div className="absolute left-[180px] mt-4">
+         <h1 className=" font-bold font-sans text-lg">Get in Touch </h1>
+         <div className="absolute right-[-65px]  w-[200px]"><p className=" font-extralight text-sm mt-4">Nunc erat cursur tellus gravida</p></div>
+         </div>
+         {/* <div className="mt-12  ">
+  //           <form>
+  //             <label className="absolute left-[10px] font-bold" for="fName">
+  //               First Name:
+  //             </label>
+  //             <br></br>
+  //             <input
+  //               className="absolute left-[10px] mt-2 border border-blue-600 w-[450px] rounded-md"
+  //               type="text"
+  //               placeholder="Please Enter First Name"
+  //             />
+  //             <br></br>
+  //             <label
+  //               className="absolute left-[10px] mt-3 font-bold"
+  //               for="lname"
+  //             >
+  //               Last name:
+  //             </label>
+  //             <br></br>
+  //             <input
+  //               className="absolute left-[10px] mt-4 border border-blue-600 w-[450px] rounded-md"
+  //               type="text"
+  //               placeholder="Please Enter First Name"
+  //             ></input>
+
+  //             <label
+  //               className="absolute left-[10px] mt-12 font-bold"
+  //               for="lname"
+  //             >
+  //               Email:
+  //             </label>
+  //             <br></br>
+  //             <input
+  //               className="absolute left-[10px] mt-14 border border-blue-600 w-[450px] rounded-md"
+  //               type="text"
+  //               placeholder="Please Enter First Name"
+  //             ></input>
+
+  //             <label
+  //               className="absolute left-[10px] mt-[90px] font-bold"
+  //               for="lname"
+  //             >
+  //               Phone Number:
+  //             </label>
+  //             <br></br>
+  //             <input
+  //               className="absolute left-[10px] mt-[97px] border border-blue-600 w-[450px] rounded-md"
+  //               type="text"
+  //               placeholder="Please Enter First Name"
+  //             ></input>
+  //           </form>
+  //           <button className="font-bold text-black bg-blue-500 absolute bottom-[-200px] py-2 px-[200px] rounded-lg left-2">
+  //             Submit
+  //           </button>
+  //         </div> */}
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default Contact;
